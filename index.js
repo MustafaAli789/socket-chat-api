92fb89d8a7e6ed2a8ee278714fe671ca1208bb02
Mustafa Ali <alimustafa0987@gmail.com>
2020-02-03 20:19:21 -0500

    socket listener added to be able to send message to a room as well as emitter to send message to front end

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket)=>{ //the socket in the param is a specific instance of client socket
-    //socket.on can take a callback function as well as third param which can be passed from client side
+
+    //socket.on can take a callback function as a second param in the arrow function which can be passed from client side
     //good for error handling/running a method after the event has been emmitted from client side
     socket.on('join', ({ name, room }, callback)=>{ 
         const { error, user } = addUser({ id:socket.id, name, room });
 
         if (error) return callback(error);
 
+        //here we are emiting a message from the backend to the fron end
         //tells users hes welcome to chat
         socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
         
@@ -31,7 +33,16 @@ io.on('connection', (socket)=>{ //the socket in the param is a specific instance
         socket.join(user.room);
 
         callback();
-    })
+    });
+
+    //here we are waiting on an event from the front end (front end will emit)
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id) //client instance socket (specific user)
+        io.to(user.room).emit('message', {user:user.name, text:message });
+
+        callback()
+    });
+
     socket.on('disconnect',()=>{
         console.log('user disconnected');
     });
