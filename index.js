e7681a95475fa777009982bed21e1590cb770421
Mustafa Ali <alimustafa0987@gmail.com>
2020-02-03 19:53:08 -0500

    socket join event created

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket)=>{
-    console.log('new user connected')
+io.on('connection', (socket)=>{ //the socket in the param is a specific instance of client socket
+    console.log('new user connected');
+    //socket.on can take a callback function as well as third param which can be passed from client side
+    //good for error handling/running a method after the event has been emmitted from client side
+    socket.on('join', ({ name, room }, callback)=>{ 
+        console.log(name +" " + room)
+    })
+    socket.on('disconnect',()=>{
+        console.log('user disconnected');
+    });
 });
 
 app.use(router)
