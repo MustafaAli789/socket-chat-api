82fcb6520c13fc8014a80debdf93135ec13eb1a9
Mustafa Ali <alimustafa0987@gmail.com>
2020-02-11 22:11:33 -0500

    fix up backend code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
 const router = require('./router');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 const app = express();
 app.use(cors());
@@ -26,12 +26,12 @@ io.on('connection', (socket)=>{ //the socket in the param is a specific instance
 
         //here we are emiting a message from the backend to the fron end
         //tells users hes welcome to chat
-        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+        socket.emit('message', { type: "Message", user: 'admin', payload: `${user.name}, welcome to the room ${user.room}` });
         
         //broadcast allows emitin an event to all other sockets connected to the server
         //.to to specify the room
         //overall tells all other members of the room user has joined
-        socket.broadcast.to(user.room).emit('message', { user:'admin', text:`${user.name} has joined!` })
+        socket.broadcast.to(user.room).emit('message', { type: "Message", user:'admin', payload:`${user.name} has joined!` })
 
         socket.join(user.room);
 
@@ -43,15 +43,16 @@ io.on('connection', (socket)=>{ //the socket in the param is a specific instance
     //here we are waiting on an event from the front end (front end will emit)
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id) //client instance socket (specific user)
-        io.to(user.room).emit('message', {user:user.name, text:message }); //sending message to fron end of all connected sockets
+        io.to(user.room).emit('message', {type:message.type, user:user.name, payload:message.payload }); //sending message to fron end of all connected sockets
         callback()
     });
 
     socket.on('disconnect',()=>{
+        console.log("receiving")
         const user = removeUser(socket.id);
         if (user) {
             io.to(user.room).emit('roomData', {room:user.room, users:getUsersInRoom(user.room)}); //updating users in room when someone leaves
-            io.to(user.room).emit('message', {user: 'admin', text:`${user.name} has left`})
+            io.to(user.room).emit('message', {type: "Messag", user: 'admin', payload:`${user.name} has left`})
         }
     });
 });
