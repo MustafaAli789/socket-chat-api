8f05d0563cd921ca775f2fcbe107fa1f3bc321b7
Mustafa Ali <alimustafa0987@gmail.com>
2020-02-11 22:19:58 -0500

    changed deafualt port to 5000

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
 const router = require('./router');
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 const app = express();
 app.use(cors());
